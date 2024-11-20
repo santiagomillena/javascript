29d23704762be3a18726f1a387fb0a6304434f52
santiagomillena <santiagomillena@gmail.com>
2024-11-20 16:17:23 -0400

    Condição simples

---
+++ exercicios/ex008/ex008.js
@@ -0,0 +1,6 @@
+var vel = 45
+console.log(`Você está á ${vel}Km/h`)
+if (vel > 60) { //Condição simples
+    console.log('Você ultrapassou o limite de velocidade. MULTADO')
+    }
+console.log('Dirija sempre com cuidado')
