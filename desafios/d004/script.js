1cae9f1645081903549ffbe297c1247bcef40337
santiagomillena <santiagomillena@gmail.com>
2024-11-26 00:03:55 -0400

    tab

--- desafios/d004/script.js
+++ desafios/d004/script.js
@@ -7,13 +7,21 @@ document.getElementById('inputNumber').addEventListener('keydown',function(event
 function verificar() {
     let num = document.getElementById('inputNumber')
     let tab = document.getElementById('seltab')
-    let result = document.getElementById('result')
 
     if (num.value.trim() === '') {
         alert('Por favor, digite um número')
         return
     } else {
         let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ""
+        while (c <= 10){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
     }
 
 }
