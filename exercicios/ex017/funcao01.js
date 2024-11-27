d6d9d62267b1694afae1ff18075bfe2bac6996eb
santiagomillena <santiagomillena@gmail.com>
2024-11-27 16:10:50 -0400

    funcoes

---
+++ exercicios/ex017/funcao01.js
@@ -0,0 +1,11 @@
+function parimpar (n) {
+    if (n % 2 == 0) {
+        return 'par'
+    } else {
+        return 'impar'
+    }
+}
+
+console.log (parimpar(233))
+//let res = parimpar (11)
+//console.log (res)
---
+++ exercicios/ex017/funcao02.js
@@ -0,0 +1,5 @@
+function soma (num1=0, num2=0){
+    return num1+num2
+}
+
+console.log (soma(7))
